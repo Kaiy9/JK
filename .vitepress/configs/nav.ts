b5dc45eac54ffc3065ac4eedab29c78b379fa11e
Kaiy9 <dejiuzhidao@gmail.com>
2025-08-07 17:57:28 +0800

    更新了网站的最新内容

--- .vitepress/configs/nav.ts
+++ .vitepress/configs/nav.ts
@@ -72,7 +72,7 @@ export const nav: DefaultTheme.Config['nav'] = [
           { text: '测速工具', link: '/工具/测速工具' },
           { text: '在线工具', link: '/工具/在线工具' },
           { text: '开发工具', link: '/工具/开发工具' },
-          { text: '好物分享', link: '/工具/好物分享' },
+          { text: '', link: '/工具/好物分享' },
           { text: '我的装备', link: '/工具/我的装备' },
           { text: '开发工具', link: '/工具/开发工具' },
           { text: '', link: '/工具/免费机场' },
