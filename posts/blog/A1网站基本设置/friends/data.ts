f47eeaae8df0cfe285914d4ad9b088bed33581b3
Kaiy9 <dejiuzhidao@gmail.com>
2025-08-08 17:40:05 +0800

    更新了网站的最新内容

---
+++ posts/blog/A1网站基本设置/friends/data.ts
@@ -0,0 +1,39 @@
+import type { NavData } from '../../.vitepress/theme/types'
+
+export const NAV_DATA: NavData[] = [
+  {
+    title: '',
+    items: [
+      {
+        icon: 'https://tinypng.com/images/apple-touch-icon.png',
+        title: 'TinyPNG',
+        desc: '在线图片压缩工具',
+        link: 'https://tinypng.com'
+      },
+      {
+        icon: 'https://devtool.tech/logo.svg',
+        title: '开发者武器库',
+        desc: '开发者武器库，做开发者最专业最好用的专业工具箱',
+        link: 'https://devtool.tech'
+      },
+     {
+        icon: 'https://devtool.tech/logo.svg',
+        title: '开发者武器库',
+        desc: '开发者武器库，做开发者最专业最好用的专业工具箱',
+        link: 'https://devtool.tech'
+      },
+     {
+        icon: 'https://devtool.tech/logo.svg',
+        title: '开发者武器库',
+        desc: '开发者武器库，做开发者最专业最好用的专业工具箱',
+        link: 'https://devtool.tech'
+      },
+      {
+        icon: 'https://www.sojson.com/sojson/favicon16.png',
+        title: 'SoJson在线解析',
+        desc: 'JSON在线解析及格式化验证',
+        link: 'https://www.sojson.com'
+      },
+    ]
+  }
+]
